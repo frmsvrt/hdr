cf193a240f74bfb65ab2c68510a1e9d099faadbd
Kravchenko Fyodor <frmsvrt0@gmail.com>
2018-09-03 23:11:42 +0800

    we do not need exiftool

--- readData.m
+++ readData.m
@@ -1,24 +1,27 @@
-function [images, exposureTime] = readData(folder)
+function [images, exposureTimes] = readData(folder)
 % function readData
 % input folder dir
 % read images from the folder and their exposure time
 
-images = [];
-exposureTime = [];
-files = dir([folder, '/*.jpg']);
-fnames = [folder, '/', files(1).name];
-info = imfinfo(fnames);
+if folder(end) ~= '/'
+    folder = [folder '/'];
+end
+
+files = dir([folder, '*.tif']); % TODO: update file format to be robust
+
+info = imfinfo([files(1,1).folder '/' files(1,1).name]);
 number = length(files);
-images = zeros(info.Height, info.Width, info.NumberOfSamples, number);
+images = zeros(info.Height, info.Width, 3, number);
 exposureTimes = zeros(number, 1);
 
 for i=1:number
-    fname = [folder, '/', files(1).name];
+    fname = [folder, '/', files(i, 1).name];
+    inf = imfinfo(fname);
+    dc = inf.DigitalCamera;
     img = imread(fname);
     images(:, :, :, i) = img;
     
-    exif = getexif(fname);
-    exposureTimes(i) = exif.ExposureTime;
+    exposureTimes(i) = dc.ExposureTime;
 end
 
 end
